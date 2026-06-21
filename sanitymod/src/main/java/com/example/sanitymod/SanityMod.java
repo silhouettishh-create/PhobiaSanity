b61877bb633109af68efd4250795fcb46f5af62f
silhouettishh-create <silhouettishh@gmail.com>
2026-06-22 06:42:25 +0700

    Create SanityMod.java

---
+++ sanitymod/src/main/java/com/example/sanitymod/SanityMod.java
@@ -0,0 +1,15 @@
+package com.example.sanitymod;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+
+public class SanityMod implements ModInitializer {
+
+    public static final String MOD_ID = "sanitymod";
+
+    @Override
+    public void onInitialize() {
+        PhobiaAssigner.register();
+        ServerTickEvents.END_SERVER_TICK.register(SanityTickHandler::onEndTick);
+    }
+}
